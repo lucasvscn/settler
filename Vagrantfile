05582e86b564a37462ba92ac419a6485fffd9254
Lucas Vasconcelos <lucas@vscn.co>
2017-03-16 22:40:28 -0300

    settings updated

--- Vagrantfile
+++ Vagrantfile
@@ -1,21 +1,19 @@
-VAGRANTFILE_API_VERSION = "2"
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
 
-Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
-	# Configure The Box
-	config.vm.box = "centos/7"
-	config.vm.hostname = "settler"
+Vagrant.configure("2") do |config|
+  config.vm.box = "centos/7"
 
-	# Don't Replace The Default Key https://github.com/mitchellh/vagrant/pull/4707
-	config.ssh.insert_key = false
+  config.vm.network "forwarded_port", guest: 80, host: 8000
+  config.vm.network "forwarded_port", guest: 3306, host: 33060
+  config.vm.network "forwarded_port", guest: 9000, host: 9000
 
-	config.vm.provider "virtualbox" do |vb|
-		vb.customize ["modifyvm", :id, "--memory", "1024"]
-		vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
-		vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
-	end
+  # Don't Replace The Default Key https://github.com/mitchellh/vagrant/pull/4707
+  config.ssh.insert_key = false
 
-	config.vm.synced_folder './', '/vagrant', disabled: true
+  config.vm.synced_folder './', '/vagrant', disabled: true
 
-	# Run The Base Provisioning Script
-	config.vm.provision 'shell', path: './scripts/provision.sh'
+  config.vm.provision 'shell', path: './scripts/update.sh'
+  config.vm.provision :reload
+  config.vm.provision 'shell', path: './scripts/provision.sh'
 end
--- scripts/provision.sh
+++ scripts/provision.sh
@@ -1,105 +1,55 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-yum clean all
+# Install extra repositories
 
 yum -y install epel-release
+rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-EPEL-7
 
-rpm -Uvh https://centos7.iuscommunity.org/ius-release.rpm
+yum -y install https://centos7.iuscommunity.org/ius-release.rpm
+rpm --import /etc/pki/rpm-gpg/IUS-COMMUNITY-GPG-KEY
 
+# Install dev & utils
 
 yum -y install bash-completion git vim curl htop unzip rar \
 sqlite dos2unix gcc make python-pip poppler-utils man \
 kernel-devel-$(uname -r) kernel-headers-$(uname -r) dkms
 
-# Install VirtualBox Guess Aditions
-vboxga_version='5.0.14'
-isofile="VBoxGuestAdditions_$vboxga_version.iso"
-url="http://download.virtualbox.org/virtualbox/$vboxga_version/$isofile"
-curl -L -o /home/vagrant/$isofile $url
+# Set my timezone
 
-mount -o loop /home/vagrant/$isofile /mnt
-sh /mnt/VBoxLinuxAdditions.run
-umount /mnt
-rm -rf /home/vagrant/$isofile
+ln -sf /usr/share/zoneinfo/America/Sao_Paulo /etc/localtime
 
-# Set My Timezone
-
-ln -sf /usr/share/zoneinfo/America/Recife /etc/localtime
-
-
-chmod 755 /home/vagrant/
-
-curl -o /usr/local/etc/git_prompt https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh
-
-cat << EOT >> /home/vagrant/.bashrc
-
-# Enable bash_completion
-if [ -f /usr/share/bash-completion/bash_completion ]; then
-    . /usr/share/bash-completion/bash_completion
-fi
-
-# Enable bash_completion
-if [ -f /usr/local/etc/git_prompt ]; then
-    . /usr/local/etc/git_prompt
-fi
-
-# Git
-alias g='git'
-complete -o default -o nospace -F _git g
-
-# prompt displays current branch
-PS1=\$PS1'\\[\\e[33m\\]\$(__git_ps1 "(%s) ")\\[\\e[0m\\]'
-EOT
-
-
-# Apache
+# Apache 2.4
 
 yum -y install httpd httpd-tools
 
-# disable 'welcome' page
-sed -i -e '/LocationMatch/,+14 s/^/#/' /etc/httpd/conf.d/welcome.conf
+systemctl enable httpd
+systemctl start httpd
 
-# https://github.com/mitchellh/vagrant/issues/351#issuecomment-1339640
-sed -i -r 's/#EnableSendfile/EnableSendfile/g' /etc/httpd/conf/httpd.conf
+# Install PHP 5.6
 
-# Install NodeJS
-
-curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
-yum -y install nodejs
-
-npm install -g grunt-cli
-npm install -g gulp
-npm install -g bower
-
-# Install PHP
-
-yum -y install php56u php56u-pdo php56u-mssql php56u-mbstring \
-php67u-ldap php56u-mcrypt php56u-pecl-apcu php56u-pecl-memcache \
-php56u-pecl-memcached php56u-pecl-xdebug php56u-bcmath
+yum -y install php56u php56u-bcmath php56u-cli php56u-gd php56u-imap \
+php56u-ioncube-loader php56u-ldap php56u-mbstring php56u-mcrypt php56u-mssql \
+php56u-mysqlnd php56u-opcache php56u-pdo php56u-pecl-apcu php56u-pecl-geoip \
+php56u-pecl-imagick php56u-pecl-memcache php56u-pecl-memcached \
+php56u-pecl-xdebug php56u-soap php56u-xml php56u-xmlrpc
 
 # Install Composer
 
-curl -sS https://getcomposer.org/installer | php
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+
 mv composer.phar /usr/local/bin/composer
 
-# Add Composer Global Bin To Path
-
 printf "\nPATH=\"$(sudo su - vagrant -c 'composer config -g home 2>/dev/null')/vendor/bin:\$PATH\"\n" | tee -a /home/vagrant/.bashrc
-
-# Install Laravel Envoy & Installer
-
-sudo su vagrant <<'EOF'
-/usr/local/bin/composer global require "laravel/envoy=~1.0"
-/usr/local/bin/composer global require "laravel/installer=~1.1"
-EOF
 
 # Set Some PHP CLI Settings
 
 sed -i "s/error_reporting = .*/error_reporting = E_ALL/" /etc/php.ini
 sed -i "s/display_errors = .*/display_errors = On/" /etc/php.ini
 sed -i "s/memory_limit = .*/memory_limit = 512M/" /etc/php.ini
-sed -i "s/;date.timezone.*/date.timezone = America\/Recife/" /etc/php.ini
-
+sed -i "s/;date.timezone.*/date.timezone = America\/Sao_Paulo/" /etc/php.ini
 
 echo "xdebug.remote_enable = 1" >> /etc/php.d/20-xdebug.ini
 echo "xdebug.remote_connect_back = 1" >> /etc/php.d/20-xdebug.ini
@@ -108,16 +58,31 @@ echo "xdebug.max_nesting_level = 512" >> /etc/php.d/20-xdebug.ini
 
 echo "<?php phpinfo();" | tee /var/www/html/info.php
 
+# MySQL 5.7
+
+# rpm -ivh https://dev.mysql.com/get/mysql57-community-release-el7-9.noarch.rpm
+# yum -y install mysql-server
+
+# systemctl enable mysqld
+# systemctl start mysqld
+
+# password=`grep 'temporary password' /var/log/mysqld.log | sed -e 's/^.*root@localhost: //'`
+# mysql --user="root" --password="$password" -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'secret';"
+# mysql --user="root" --password="secret" -e "CREATE USER 'root'@'0.0.0.0' IDENTIFIED BY 'secret';"
+# mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO root@'0.0.0.0' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
+# mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'root'@'0.0.0.0' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
+# mysql --user="root" --password="secret" -e "GRANT ALL ON *.* TO 'root'@'%' IDENTIFIED BY 'secret' WITH GRANT OPTION;"
+# mysql --user="root" --password="secret" -e "FLUSH PRIVILEGES;"
+
+# systemctl restart mysqld
+
+# Caching & Queues
+
 yum -y install libmemcached libmemcached-devel memcached beanstalkd
 
+# Cleaning up
 
-# Install Supervisord
+yum clean all
 
-easy_install supervisord
-echo_supervisord_conf > /etc/supervisord.conf
-curl -o /etc/systemd/system/supervisord.service https://raw.githubusercontent.com/Supervisor/initscripts/master/centos-systemd-etcs
-
-
-# Zero out the free space to save space in the final image:
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -f /EMPTY
